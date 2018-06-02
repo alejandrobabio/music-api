07d12f14ccb0f4cbf2ecd9a486f62f2d1ca982a4
Alejandro Babio <alejandroebabio@gmail.com>
2018-06-02 17:32:15 -0300

    Add use cases for delete album

---
+++ app/use_cases/delete_album.rb
@@ -0,0 +1,17 @@
+module MusicAPI
+  module UseCases
+    class DeleteAlbum
+      attr_reader :model_class
+      private :model_class
+
+      def initialize(model_class)
+        @model_class = model_class
+      end
+
+      def call(id)
+        model_class.destroy(id)
+        ''
+      end
+    end
+  end
+end
