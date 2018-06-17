9f35881efe3e8d476c7d3ca43e30eccf66707ba2
Alejandro Babio <alejandroebabio@gmail.com>
2018-06-16 23:29:48 -0300

    Add use case for deleting a song

---
+++ app/use_cases/delete_song.rb
@@ -0,0 +1,17 @@
+module MusicAPI
+  module UseCases
+    class DeleteSong
+      attr_reader :model_class
+      private :model_class
+
+      def initialize(model_class)
+        @model_class = model_class
+      end
+
+      def call(id)
+        model_class.destroy(id)
+        ''
+      end
+    end
+  end
+end
---
+++ spec/use_cases/delete_song_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require_relative '../../app/use_cases/delete_song'
+
+module MusicAPI
+  RSpec.describe UseCases::DeleteSong do
+    let(:model_class) { double }
+    let(:use_case) { described_class.new(model_class) }
+    let(:id) { 123 }
+
+    context 'when it deletes record' do
+      before do
+        expect(model_class).to receive(:destroy).with(id)
+      end
+
+      it { expect(use_case.call(id)).to be_empty }
+    end
+
+    context "when it can't delete the record" do
+      before do
+        allow(model_class).to receive(:destroy).with(id)
+          .and_raise(StandardError.new)
+      end
+
+      it 'raises an exception' do
+        expect {
+          use_case.call(id)
+        }.to raise_exception(StandardError)
+      end
+    end
+  end
+end
